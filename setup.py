cdbaaaa0755e83eae4a2034e4ae2439392b1c1d0
darshanime <deathbullet@gmail.com>
2017-03-02 10:08:54 +0530

    upload to pypi

---
+++ setup.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+from distutils.core import setup
+
+setup(
+    name = "sandpiles",
+    packages = ["sandpiles"],
+    version = "0.2",
+    description = "Generalized sandpiles for eyegasm",
+    author = "Darshan Chaudhary",
+    author_email = "deathbullet@gmail.com",
+    url = "https://github.com/darshanime/sandpiles",
+    download_url = "https://github.com/darshanime/sandpiles/archive/0.1.tar.gz",
+    keywords = ["sandpiles", "visualization"],
+    license = "LICENSE.txt",
+    classifiers = [],
+    install_requires=[
+        "matplotlib==2.0.0", "numpy==1.12.0"
+    ]
+)
